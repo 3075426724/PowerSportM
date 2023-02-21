6e6ca329b7c46cd404b80531a56b43097d84f20d
laopaoer <laopaoer@outlook.com>
2023-02-21 12:09:47 +0800

    CR

--- miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-3go08kosfd9d453c","alias":"cloud1"}]
-const isMac = false
-module.exports = {
-    envList,
-    isMac
-}
--- uploadCloudFunction.bat
+++
@@ -1,1 +0,0 @@
-"D:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-3go08kosfd9d453c --n quickstartFunctions --r --project "D:\Run\git\tongxuelu" --report_first --report
